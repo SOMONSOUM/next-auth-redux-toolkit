6dd4368f59d79cef015402c5acb0f998aa3ffc5a
SOM ON SOUM <somon.soum9@gmail.com>
2023-06-14 17:29:26 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/styles/globals.css";
+import type { AppProps } from "next/app";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "~/features/auth";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
